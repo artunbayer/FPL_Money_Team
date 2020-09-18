24944e1c27648cf869e11926c7f11da824b07102
bohan <bw22@rice.edu>
2020-09-18 17:27:34 -0500

    first commit

--- data.py
+++ data.py
@@ -8,6 +8,6 @@ url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 
 # Use the requests package to make a GET request from the API endpoint
 r = requests.get(url)
-
+import collections
 # 
 json = r.json()
