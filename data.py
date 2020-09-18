87fb9d68577b1210effc658cb4b031f42741cda0
artunbayer <artunbayer@gmail.com>
2020-09-18 17:19:38 -0500

    url

---
+++ data.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+import numpy as np
+
+
+# API URL
+url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+
+# Use the requests package to make a GET request from the API endpoint
+r = requests.get(url)
+
+# 
+json = r.json()
