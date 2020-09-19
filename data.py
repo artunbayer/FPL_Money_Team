53dee6226cb38e2e7646b951ed953f5048997e76
artunbayer <artunbayer@gmail.com>
2020-09-18 20:02:33 -0500

    processed the data

--- data.py
+++ data.py
@@ -2,19 +2,22 @@ import requests
 import pandas as pd
 import numpy as np
 
-
-# API URL
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
-
-# Use the requests package to make a GET request from the API endpoint
 r = requests.get(url)
-
-# transform that request into a json object:
 json = r.json()
+elements_types_df = pd.DataFrame(json['element_types'])
 
 
-teams_df = pd.DataFrame(json['teams'])
-elements_df = pd.DataFrame(json['elements'])
-element_stats_df = pd.DataFrame(json['element_stats'])
+raw_data_frames = []
+clean_data_frames = []
 
-print(json.keys())
+for i in range(1, 5):
+    raw_data_frame = pd.read_csv('./data/' + str(i) +'_raw.csv', encoding='latin-1')
+    clean_data_frame = pd.read_csv('./data/' + str(i) +'.csv', encoding='latin-1')
+    raw_data_frames.append(raw_data_frame)
+    clean_data_frames.append(clean_data_frame)
+
+
+for i in range(len(raw_data_frames)):
+    clean_data_frames[i]['position'] = raw_data_frames[i].element_type.map(elements_types_df.set_index('id').singular_name)
+
